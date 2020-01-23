b57c20a4c098a5f9852fef6cc3397cc1a87fd8ad
Josh Morris <jmo0624@gmail.com>
2020-01-23 03:00:03 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -28,7 +28,7 @@ def can_be_created_in_a_block(args = {title: "Home ALone", release_date: 1990})
   # title == "Home Alone"
   # release_date == 1990
   Movie.create do |m|
-    m.title = "Home Alone"
+    m.title = 
     m.release_date = 1990
   end
   
